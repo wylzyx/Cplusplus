d7b61143d103939c22b83fa0eb0be56d88f250e4
wylzyx <wanyu990124@163.com>
2019-11-06 21:42:59 +0800

    2019-11-06

---
+++ string.11.5.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class String
+{
+public:
+	/*String()
+		:_str(new char[1])
+		{
+		*_str = '\0';
+		}
+
+		String(char* str)
+		:_str(new char[strlen(str) + 1])
+		{
+		strcpy(_str, str);
+		}
+		*/
+
+	String(char* str = "")
+		:_str(new char[strlen(str) + 1])
+	{
+		strcpy(_str, str);
+	}
+
+	/*String(const String& str)
+		:_str(new char[strlen(str._str) + 1])
+	{
+		strcpy(_str, str._str);
+	}*/
+
+	String(const String& str)
+		:_str(nullptr)
+	{
+		String tmp(str._str);
+		swap(_str, tmp._str);
+	}
+
+	/*String& operator=(const String& str)
+	{
+		if (this != &str)
+		{
+			delete[] _str;
+			_str = new char[strlen(str._str) + 1];
+			strcpy(_str, str._str);
+		}
+		return *this;
+	}*/
+
+	String& operator=(String str)
+	{
+		//String tmp(str._str);
+		swap(_str, str._str);
+		return *this;
+	}
+	
+
+	const char* c_str() const
+	{
+		return _str;
+	}
+
+	~String()
+	{
+		if (_str)
+			delete[] _str;
+	}
+
+private:
+	char* _str;
+};
+
+void testString1()
+{
+	String s;
+	/*string s2;
+	cout << s.c_str() << endl;
+	cout << s2.c_str() << endl;
+	cout << (int*)s.c_str() << endl;
+	cout << (int*)s2.c_str() << endl;*/
+	/*cout << s2.capacity() << endl;
+	cout << s2.size() << endl;*/
+	char* str = new char[10];
+	strcpy(str, "hello");
+	String s3(str);
+
+	delete[] str;
+
+	char sarr[] = "\0";  // \0 \0
+	char sarr2[] = "\\0"; // \  0  \0
+	char sarr3[] = "\000"; // \0 \0 
+
+}
+
+void testString2()
+{
+	String s1("hello");
+	String s2 = s1;  // 拷贝构造
+	s2 = s1; // 赋值运算符
+}
+
+int main()
+{
+	//testString1();
+	testString2();
+	return 0;
+}
